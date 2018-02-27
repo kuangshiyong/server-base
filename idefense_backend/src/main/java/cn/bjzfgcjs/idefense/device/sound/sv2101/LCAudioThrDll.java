9852339e38c1ccec58405b4ec395eea5d8c3d808
yixin.zhongshenghui <zhongshenghui@yixin.im>
2018-02-28 00:56:22 +0800

    更新音频卡SDK

--- idefense_backend/src/main/java/cn/bjzfgcjs/idefense/device/sound/sv2101/LCAudioThrDll.java
+++ idefense_backend/src/main/java/cn/bjzfgcjs/idefense/device/sound/sv2101/LCAudioThrDll.java
@@ -17,17 +17,10 @@ public interface LCAudioThrDll extends StdCallLibrary {
             LCAudioThrDll.class);
 
     /*定义网络通信方式*/
-    public final static int cUnicast = 0;	//单播
+    public final static int cUnicast = 0;	    //单播
     public final static int cMulticast	= 1;	//组播
     public final static int cBroadcast	= 2;	//广播
-
-
-    public final static int AUDIO__NOT_EXIST = -1;
-    public final static int AUDIO_TYPE_ERR  = 0;
-    public final static int AUDIO_TYPE_MP3  = 1;
-    public final static int AUDIO_TYPE_WAV  = 2;
-    public final static int AUDIO_TYPE_WMA  = 3;
-    public final static int AUDIO_TYPE_MP2  = 4;
+    public final static int cMulticast2	= 3;	//二类组播
 
     public final static int SCR_TYPE_FILE = 0;			//数据来源是文件
     public final static int SCR_TYPE_AUDIOCARD = 1;	    //数据来源是声卡
@@ -35,17 +28,17 @@ public interface LCAudioThrDll extends StdCallLibrary {
     public final static int SCR_TYPE_STREAM = 3;		//数据来源是数据流//V2.0.3.0
 
     /*定义函数返回值*/
-    public final static int R_OK = 0;		//成功
-    public final static int ERR_PARAM = -1;		//参数错误
-    public final static int ERR_OPT   = -2;		//函数执行错误
+    public final static int R_OK = 0;		        //成功
+    public final static int ERR_PARAM = -1;		    //参数错误
+    public final static int ERR_OPT   = -2;		    //函数执行错误
     public final static int ERR_SOCKET = -3;		//socket操作失败
-    public final static int ERR_CODEC = -4;	//初始化编解码器失败。
-
+    public final static int ERR_CODEC = -4;	        //初始化编解码器失败。
     public final static int ERR_MEM_FAULT = -6;		//读取文件失败
-    public final static int ERR_FILEFORMAT_FAULT = -7; //
+    public final static int ERR_FILEFORMAT_FAULT = -7;
     public final static int ERR_BAD_BITRATE = -8;
-    public final static int ERR_DECODE_DISABLE = -9;	//应用程序禁用解码功能，而播放此文件需要解码
-    /*Windows 消息定义*/
+    public final static int ERR_DECODE_DISABLE = -9; //应用程序禁用解码功能，而播放此文件需要解码
+
+    /* Windows 消息定义 */
 //exception
     public final static int WM_USER = 0x400;
     //public final static int WM_MSG_EXCEPTION	= (WM_USER+100)
@@ -55,14 +48,31 @@ public interface LCAudioThrDll extends StdCallLibrary {
     public final static int WM_MSG_AUDIOPOWER = (WM_USER+104);
     public final static int WM_MSG_SOUNDCARD  =	(WM_USER+105);
 
+    //_PlayParam.CtrlByte控制字定义
+    // 重采样选项，在录播模式时，因计算机采样与播放设备回放频率的差异，可能导致时延
+    // 选择重采样选项，动态库自动处理这个问题,但带来轻微的失真。
+    public final static int OPT_RESAMPLE = 0X0001;
+    public final static int OPT_STREAM   = 0X0002;
+
+    //禁用解码器，因为解码器不是线程安全的，故有些多线程应用需要禁用，以保证稳定。
+    public final static int OPT_DECODE_DISABLE = 0X0004;
+
+    // 音频文件支持的格式
+    public final static int AUDIO__NOT_EXIST = -1;
+    public final static int AUDIO_TYPE_ERR  = 0;
+    public final static int AUDIO_TYPE_MP3  = 1;
+    public final static int AUDIO_TYPE_WAV  = 2;
+    public final static int AUDIO_TYPE_WMA  = 3;
+    public final static int AUDIO_TYPE_MP2  = 4;
+
 
     public class _FileInfo extends Structure {
 
-        public int Duration;  //播放时长
+        public int Duration;   //播放时长
 
         public int SampleRate; //采样率
 
-        public int BitRate; //比特率
+        public int BitRate;    //比特率
 
         public _FileInfo() {
             super();
@@ -91,50 +101,47 @@ public interface LCAudioThrDll extends StdCallLibrary {
     public class _PlayParam extends Structure {
         public HWND hWnd; //主窗口的句柄，如果不为0，则线程有事件会向该窗口发送消息
 
-        public int Priority; //优先级
+        public int Priority;   //优先级
 
         public int MultiGroup; //多播组号
 
-        public int CastMode; //传输模式，单播，多播，广播
+        public int CastMode;   //传输模式，单播，多播，广播
 
-        public NativeLong IP; // 如果是广播和多播，此参数是源网卡的IP，如果此地址为0，则由系统决定使用哪个网卡，如果是单播，这是目标设备的ip地址。
+        public NativeLong IP;  // 如果是广播和多播，此参数是源网卡的IP，如果此地址为0，则由系统决定使用哪个网卡，如果是单播，这是目标设备的ip地址。
 
-        public int Volume; //播放音量取值0～100
+        public int Volume;   //播放音量取值0～100
 
         public int Tone;    //音调
 
-        public int Treble;   //高音频率
+        public int Treble;  //高音频率
 
         public int Bass;    //低音频率
 
-        public int   Treble_En;		//高音增益
+        public int Treble_En;   //高音增益
 
-        public int   Bass_En;		//低音增益
+        public int Bass_En;		//低音增益
 
         public int SourceType;	//输入源，0为文件，1为声卡
 
-        public int OptionByte;
+        public int OptionByte;  //选项字，默认为0; bit0=1
 
         public int DeviceID;		//音频输入ID号 1～N
 
-        //        public byte[] MuxName = new byte[64]; //混音器的通道名字
+        // 混音器使用默认首选设备，不再设置。
 
-        public int MaxBitrate;
+        public int MaxBitrate;  //允许最大的比特率组合，如果源文件高于此比特率，将被重压缩至此比特率。
 
-        public int[] Options = new int[15];
-        public int nChannels; //采样的通道 1～2 CodecType
+        public int[] Options = new int[15]; //选项
+
+        public int nChannels;   //采样的通道 1～2 CodecType
 
         public int nSamplesPerSec; //采样频率 8K，11.025K,22.05K,44.1K
 
         public int AudioBufferLength; //Audio数据的长度
 
-        public Pointer AudioBuf;  //Audio数据的指针
+        public Pointer AudioBuf;      //Audio数据的指针
 
         public int[] PrivateData = new int[128]; //私有信息，lc_init初始化后，用户不能修改里面的内容。
-
-        public _PlayParam() {
-            super();
-        }
 
         @Override
         protected List<String> getFieldOrder() {
@@ -148,18 +155,17 @@ public interface LCAudioThrDll extends StdCallLibrary {
 
         public static class ByValue extends _PlayParam implements Structure.ByValue {
         };
-
     }
 
     public class _WaveInInfo extends Structure {
 
-        public int Index;  // 输入通道的序号
+        public int Index;                   // 输入通道的序号
 
         public byte[] name = new byte[32];  //  名字
 
-        public NativeLong Formats;          //  通道数 2为立体声输入。
+        public NativeLong Formats;          //  支持的格式
 
-        public int Channels;                //  通道数 2为立体声输入。
+        public int Channels;                //  通道数；2为立体声输入。
 
         public _WaveInInfo() {
             super();
@@ -213,7 +219,7 @@ public interface LCAudioThrDll extends StdCallLibrary {
         };
     }
 
-    public _PlayParam.ByReference lc_play_getmem();
+    public Pointer lc_play_getmem();
 
     public int lc_play_freemem(_PlayParam.ByReference pParam);
 
@@ -230,6 +236,8 @@ public interface LCAudioThrDll extends StdCallLibrary {
     public int lc_seek(_PlayParam.ByReference pParam, int time);
 
     public int lc_wait(_PlayParam.ByReference pParam);
+
+    public int lc_wait_time(_PlayParam.ByReference pParam,int tv);  //tv以ms为单位
 
     public int lc_set_volume(_PlayParam.ByReference pParam, byte volume);
 
@@ -264,7 +272,8 @@ public interface LCAudioThrDll extends StdCallLibrary {
     //必须是192kbps一下mp3
     public int lc_inputdata_mp3(_PlayParam.ByReference pParam, Pointer buf, int datalen);
 
-    public int lc_get_datasize(_PlayParam.ByReference pParam);	//查询流模式下，当前的播放缓存的数据量。
+    //查询流模式下，当前的播放缓存的数据量。
+    public int lc_get_datasize(_PlayParam.ByReference pParam);
 
     public int lc_addip(_PlayParam.ByReference pParam, DWORD IP);
 
